e74f9d56860fdd2d60e5110d49f532d2e4fd1b72
Ramiro Debbe <ramirodebbe@gmail.com>
2017-01-30 15:29:27 -0500

    Removed print statements

--- routes.py
+++ routes.py
@@ -21,10 +21,10 @@ TOOLS = "pan,wheel_zoom,box_zoom,reset"
 numlines = 2
 mypalette=Spectral11[0:numlines]
 
-print('inside app')
+
 @app.route("/")
 def index():
-  print('at /')
+  
   return render_template("index.html")
 
 @app.route("/about")
@@ -33,18 +33,16 @@ def about():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
-	print('in signup')
+	
 	form = SignupForm()
 	if request.method == 'POST':
 		if form.validate() == False:
 			return render_template('signup.html', form=form)
 		else:
-			print('Stock ticker: ', form.stock_name.data)
+		
 			nameStock = 'EOD/{}'.format(form.stock_name.data)
 			stock_df = quandl.get(nameStock, index_col='Year')
 			tsStock = stock_df['Close']
-			print('got the data from Quandl')
-			print('Stock ticker: ', nameStock)
 
 			numlines = 2
 			mypalette=Spectral11[0:numlines]
@@ -54,7 +52,7 @@ def signup():
                         x_axis_label='date', y_axis_label = '{} daily closing value'.format(form.stock_name.data),
                         x_axis_type="datetime")
     
-			print('starting script p: ',p)
+
 
 			p.multi_line(xs=[tsStock.index.values]*numlines,
                 ys=[tsStock.values, moving_avg.values],
@@ -62,10 +60,10 @@ def signup():
                 line_width=2)
         
 			script, div = components(p)
-			print('will render graph.html')
+
 			return render_template('graph.html', script=script, div=div)
 
-			#return  "Success!"
+
 
 	elif request.method == "GET":
 		return render_template('signup.html', form=form)
